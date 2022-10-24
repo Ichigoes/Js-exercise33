ff416906618d3322a7fd72ea34ca83cc3416a859
Ichigoes <102054584+Ichigoes@users.noreply.github.com>
2022-10-24 22:58:38 +0300

    Add files via upload

---
+++ Js exercise33.js
@@ -0,0 +1,26 @@
+function fruitOrVegetable(input) {
+    food = input[0]
+
+    switch (food) {
+        case "banana":
+        case "apple":
+        case "kiwi":
+        case "cherry":
+        case "lemon":
+        case "grapes":
+            console.log("fruit");
+            break;
+        case "tomato":
+        case "cucumber":
+        case "pepper":
+        case "carroy":
+            console.log("vegetable");
+            break;
+        default:
+            console.log("unknown");
+            break;
+    }
+
+}
+
+fruitOrVegetable(["banana"])
